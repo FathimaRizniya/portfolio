8f1e437212175654f49ba6c539e610ba8ed0f73e
FathimaRizniya <fathimarizniya792@gmail.com>
2025-10-01 23:28:21 +0530

    change the ui design

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
  
-  const cvButton = document.querySelector(".cv");
-  cvButton.addEventListener("click", () => {
+//   const cvButton = document.querySelector(".cv");
+//   cvButton.addEventListener("click", () => {
+//     const link = document.createElement("a");
+//     link.href = "mycv.pdf";  
+//     link.download = "Fathima_Risniya_CV.pdf";
+//     link.click();
+//   });
+
+  
+//   const menuBtn = document.querySelector(".menu-btn");
+//   const navLinks = document.querySelector(".nav-links");
+  
+//   menuBtn.addEventListener("click", () => {
+//     navLinks.classList.toggle("show");
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const buttonButton = document.querySelector(".button");
+  buttonButton.addEventListener("click", () => {
     const link = document.createElement("a");
     link.href = "mycv.pdf";  
     link.download = "Fathima_Risniya_CV.pdf";
@@ -16,3 +35,4 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("show");
   });
 });
+
